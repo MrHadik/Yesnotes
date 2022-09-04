f4cddfb7d71edb3e7f35729a16a7a5a95c33b099
Hardik Chavda <74814143+MrHadik@users.noreply.github.com>
2022-09-04 09:18:03 +0530

    add User in database

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -21,5 +21,5 @@ const UserSchema = new Schema({
         default: Date.now
     }
   });
-
-  module.exports = mongoose.model('user',UserSchema);
+  const User = mongoose.model('User',UserSchema);
+  module.exports = User;
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -5,17 +5,29 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { body, validationResult } = require('express-validator');
 
-router.post('/',[body('email','Enter Valid Email').isEmail(),body('passwd','Password Must be 5 Digit').isLength({ min: 5 })], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        passwd: req.body.passwd,
-      }).then(user => res.json(user))
+router.post('/creatuser', [body('email', 'Enter Valid Email').isEmail(), body('passwd', 'Password Must be 5 Digit').isLength({ min: 5 })], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  // let ch = await User.findOne({error: req.body.email});
+
+  // if (ch) {
+  //   return res.status(400).json({ error: "User alredy Registerd" });
+  // }
+  try {
     
-  })
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      passwd: req.body.passwd,
+    }).then(user => res.json(user))
+  } catch (error) {
+    return res.status(400).json({ errors: error});
+  }
+  
+
+})
 
 module.exports = router
