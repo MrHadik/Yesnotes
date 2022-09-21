b2e042c5e5bba296e333da81d5ad75cf5e47a196
Hardik Chavda <chavdah80@gmail.com>
2022-09-21 21:27:01 +0530

    added login end point

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,9 +2,11 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const { Schema } = mongoose;
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const { useInsertionEffect } = require('react');
 
 router.post('/creatuser', [body('email', 'Enter Valid Email').isEmail(), body('passwd', 'Password Must be 5 Digit').isLength({ min: 5 })], async (req, res) => {
   const errors = validationResult(req);
@@ -29,11 +31,43 @@ router.post('/creatuser', [body('email', 'Enter Valid Email').isEmail(), body('p
           name: req.body.name,
           email: req.body.email,
           passwd: hash
-        }).then(user => res.json(user))
+        })
+        const date = {user:{id:User.id}}
+        const token = jwt.sign(date, 'shhhhh');
+        res.json({token})
       });
     });
   } catch (error) {
     return res.status(400).json({ errors: error });
+  }
+})
+
+router.post('/login', [body('email', 'Enter Valid Email').isEmail(), body('passwd', 'enter password').isLength({ min: 1 })], async (req, res) => {
+  
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    let ch = await User.findOne({ email: req.body.email });
+    if (!ch) {
+      return res.status(400).json({ error: "User Not Found" });
+    }
+
+    const passcom = await bcrypt.compare(req.body.passwd,User.passwd);
+
+    if (!passcom) {
+      return res.status(400).json({ error: "Worng Password" });
+    }
+    
+    const date = {user:{id:User.id}}
+    const token = jwt.sign(date, 'shhhhh');
+    res.json({token})
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error });
   }
 })
 
